f68ca99b70b6c42f59a7919955d5029f0ac34621
itsuki <itsuki.enjyoy@gmail.com>
2024-05-25 12:41:42 +0900

    Initial Commit

---
+++ PHAssetDemo/PHAssetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PHAssetDemoApp.swift
+//  PHAssetDemo
+//
+//  Created by Itsuki on 2024/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PHAssetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
